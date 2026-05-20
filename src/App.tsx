1766b5dbd433b30f589b2c99352156284187fbf5
Mathieu Diep <mathieu.diep.95@gmail.com>
2026-05-20 18:02:51 +0200

    feat(app): route / to the Chooser page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { Chooser } from "@shared/components/Chooser";
 import { CinemaApp } from "./variants/cinema/routes";
 import { EditorialApp } from "./variants/editorial/routes";
 
@@ -7,10 +8,10 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Chooser />} />
         <Route path="/cinema/*" element={<CinemaApp />} />
         <Route path="/editorial/*" element={<EditorialApp />} />
-        <Route path="/" element={<Navigate to="/cinema" replace />} />
-        <Route path="*" element={<Navigate to="/cinema" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <Analytics />
     </BrowserRouter>
